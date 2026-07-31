4f60636cbc25b6eee49389bab00e0cf4189286f2
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2026-07-31 21:04:20 +0200

    feat: add Exo 2 variable font, update meta tags, and include robots.txt and sitemap.xml

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import '@fontsource-variable/exo-2'
 
 const app = createApp(App)
 
